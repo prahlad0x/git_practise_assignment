d3a67354f6d77819b21ba1e31116863f3858c9bd
unknown <prahladkush231@gmail.com>
2022-12-04 16:27:06 +0530

    prime_check

---
+++ prime.js
@@ -0,0 +1,13 @@
+let num = 97;
+isprime = true
+for(i=2 i<=num; i++){
+	if(num%i == 0){
+		isprime= false;
+       }
+}
+if(prime == false){
+	console.log("Not a Prime")
+}
+else{
+	console.log("Prime")
+}
