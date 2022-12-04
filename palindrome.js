109a742370fcc0d07c8da6a0300c762fb47dc61c
unknown <prahladkush231@gmail.com>
2022-12-04 16:35:46 +0530

    palindrome

---
+++ palindrome.js
@@ -0,0 +1,9 @@
+let str = "racecar";
+ispalindrome = "Palindrome;
+for(i=0; i<=str.length; i++){
+	if(str[i]!==str[str.length-1-i]){
+		ispalindrome = "Not a Palindrome"
+		break;
+	}
+}
+console.log(ispalindrome)
